42603a2b867172586851287f8e985c145fbc54f6
buithaison17 <125809724+buithaison17@users.noreply.github.com>
2026-09-08 07:17:28 +0700

    Upload bai1/src/main/java/com/example/bai1/book/BookController.java

---
+++ bai1/src/main/java/com/example/bai1/book/BookController.java
@@ -0,0 +1,20 @@
+package com.example.bai1.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/books")
+public class BookController {
+    private final BookService bookService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> findById(@PathVariable String id) {
+        return ResponseEntity.ok(bookService.findById(id));
+    }
+}
